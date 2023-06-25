692700f5f1a79652be5bfc7e2d65d241f400c188
Souhardya Das <dassouhardya79@gmail.com>
2023-06-25 17:48:10 +0530

    changes are made

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<conio.h>
 char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };			//VALUES OF SQUARE BLOCKS ASSIGNING
 int checkwin();
